a21359e56347cfa6e6393799731647259a391c91
Mason Redacted <67813185+254nm@users.noreply.github.com>
2022-08-23 01:13:50 -0400

    Strip color from messages before sending to discord

--- src/main/java/me/l2x9/chatbridge/paper/listeners/ChatListener.java
+++ src/main/java/me/l2x9/chatbridge/paper/listeners/ChatListener.java
@@ -3,6 +3,7 @@ package me.l2x9.chatbridge.paper.listeners;
 import lombok.RequiredArgsConstructor;
 import me.l2x9.chatbridge.L2X9ChatBridge;
 import me.l2x9.core.event.CheckedChatEvent;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -11,6 +12,6 @@ public class ChatListener implements Listener {
     private final L2X9ChatBridge plugin;
     @EventHandler
     public void onChat(CheckedChatEvent event) { //This event is from the core plugin and is produced by its chat listener
-        plugin.getBot().sendBridgeMessage(event.getMessage());
+        plugin.getBot().sendBridgeMessage(ChatColor.stripColor(event.getMessage()));
     }
 }
